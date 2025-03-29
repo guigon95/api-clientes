3a76a52af863c6f2b3f2d75179688493def6c4ee
guigon95 <guilherme_gon@live.com>
2025-03-29 15:54:34 -0300

    first commit

---
+++ src/main/kotlin/com/guigon/api_clientes/ApiClientesApplication.kt
@@ -0,0 +1,11 @@
+package com.guigon.api_clientes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiClientesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiClientesApplication>(*args)
+}
---
+++ src/test/kotlin/com/guigon/api_clientes/ApiClientesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guigon.api_clientes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiClientesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
